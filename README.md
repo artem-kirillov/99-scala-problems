df415ff44163c63f1c1dc1b1817a637e4f2495dd
Artem Kirillov <akirillov@griddynamics.com>
2018-01-15 00:23:12 +0300

    Add P01

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 99-scala-problems
 Solutions for 99 scala problems
+
+http://aperiodic.net/phil/scala/s-99/
--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,3 +1,17 @@
-object P01 {
+import scala.annotation.tailrec
 
+/**
+  * Find the last element of a list.
+  *
+  * Example:
+  *  scala> last(List(1, 1, 2, 3, 5, 8))
+  *  res0: Int = 8
+  */
+object P01 {
+  @tailrec
+  def last[A](list: List[A]): A = list match {
+    case x :: Nil => x
+    case _ :: xs => last(xs)
+    case Nil => throw new NoSuchElementException()
+  }
 }
